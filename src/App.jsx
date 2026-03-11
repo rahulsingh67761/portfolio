30a5811ad659207d67810477a9282ef61e498b75
Rahul <rahulsingh67761@gmail.com>
2026-03-11 21:41:32 +0530

    fix: remove embedded git repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout'
 import Home from './Home'
 import Projects from './Projects'
@@ -7,21 +7,37 @@ import Contact from './Contact'
 import About from './About'
 
 const App = () => {
-    const crudpath = createHashRouter([
+
+    let crudpath = createBrowserRouter([
         {
-            path: "/",
-            element: <Layout />,
+            path : "/",
+            element: <Layout/>,
             errorElement: <h2>Page not found ❌</h2>,
-            children: [
-                { index: true, element: <Home /> },
-                { path: "/about", element: <About /> },
-                { path: "/projects", element: <Projects /> },
-                { path: "/contact", element: <Contact /> }
+            children:[
+                {
+                index: true,
+                element: <Home />
+                },
+                {
+                    path: "/about",
+                    element: <About />
+                },
+                {
+                    path: "/projects",
+                    element: <Projects />
+                },
+                {
+                    path: "/contact",
+                    element: <Contact />
+                }
             ]
         }
-    ]);
-
-    return <RouterProvider router={crudpath} />;
+    ])
+  return (
+    <>
+    <RouterProvider router={crudpath}></RouterProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
